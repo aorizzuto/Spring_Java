50a0a7f89aff849d2b8cb2a8e0aed3b2c6c538f5
Alejandro <alejandro.omar.rizzuto@gmail.com>
2021-05-28 19:12:28 -0300

    Handling exception for User not found

--- udemycourse/src/main/java/com/example/udemycourse/user/UserResource.java
+++ udemycourse/src/main/java/com/example/udemycourse/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.example.udemycourse.user;
 
+import java.net.URI;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -20,6 +25,21 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if (user==null)
+			throw new UserNotFoundException("id: "+id);
+		return user;
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
